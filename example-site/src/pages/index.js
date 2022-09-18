8f64b59715f463e52ba15c267bc72a5308aa5df3
Oliver-BE <obaldwinedwards@gmail.com>
2022-09-17 19:46:08 -0700

    Small updates

--- example-site/src/pages/index.js
+++ example-site/src/pages/index.js
@@ -20,7 +20,7 @@ export default function IndexPage() {
                 <AboutSection sectionId="about" heading="About Me" />
                 <InterestsSection sectionId="details" heading="Skills" />
                 <ProjectsSection sectionId="features" heading="Projects" />
-                <ContactSection sectionId="github" heading="Contact" />
+                <ContactSection sectionId="contact" heading="Contact" />
             </Page>
         </>
     );
